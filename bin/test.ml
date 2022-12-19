d3b32e31f3e1c0ff379fa6a68b3557a052a909e6
José Romildo <malaquias@gmail.com>
2022-12-19 14:22:35 -0300

    Complete maxargs

--- bin/test.ml
+++ bin/test.ml
@@ -1,5 +1,6 @@
 open Ast
 
+(* print(8) *)
 let p1 = PrintStm [NumExp 8.0]
 
-(* let () = print_int (Maxargs.maxargs p1) *)
+let () = print_int (Maxargs.maxargs p1)
--- lib/maxargs.ml
+++ lib/maxargs.ml
@@ -1,7 +1,20 @@
 open Ast
 
-let rec maxargs s =
-  match s with
+let rec maxargs stm =
+  match stm with
   | CompoundStm (s1, s2) -> max (maxargs s1) (maxargs s2)
   | AssignStm (_, e) -> maxargs_exp e
   | PrintStm l -> max (List.length l) (maxargs_exp_list l)
+
+and maxargs_exp exp =
+  match exp with
+  | IdExp _ -> 0
+  | NumExp _ -> 0
+  | OpExp (_, e1, e2) -> max (maxargs_exp e1) (maxargs_exp e2)
+  | EseqExp (s, e) -> max (maxargs s) (maxargs_exp e)
+  | CallExp (_, l) -> maxargs_exp_list l
+
+and maxargs_exp_list lst =
+  match lst with
+  | [] -> 0
+  | h::t -> max (maxargs_exp h) (maxargs_exp_list t)
